3043f77b351c445703f43cb7c8e58e7dc76a77e6
Farqleet Ahmed Sheikh <farqleetahmedsheikh@users.noreply.github.com>
2024-11-11 03:47:50 +0500

    Edit python file. add comments

--- whatsapp.py
+++ whatsapp.py
@@ -1,20 +1,24 @@
 import pywhatkit
 import time
 
+# Getting user local time
 currentTime = f"Current time is {time.localtime().tm_hour} : {time.localtime().tm_min}"
 
-print(currentTime)
+print(currentTime) # Printing current local time
 
-# hour = time.localtime().tm_hour + 1
+hour = time.localtime().tm_hour + 1 # setting hours 
 
-# number = input("Enter Number with country code: (+923333333333) : ")
-# msg = input("Enter message you want to send in Loop ")
+# Getting data from user like number and message which to be send
+number = input("Enter Number with country code: (+923333333333) : ")
+msg = input("Enter message you want to send in Loop ")
 
-# print(f"Your {msg} message will start transfer from {hour} : 00 to {number} Number")
+# Printing some information about message and others
+print(f"Your {msg} message will start transfer from {hour} : 00 to {number} Number")
 
-# while True:
-#     pywhatkit.sendwhatmsg(number, msg, hour,0)
-#     hour += 1
-#     if hour == 24:
-#         hour = 0
-#     time.sleep(3590)
+# Running loop to send messages infinite times
+while True:
+    pywhatkit.sendwhatmsg(number, msg, hour,0)
+    hour += 1
+    if hour == 24:
+        hour = 0
+    time.sleep(3590) # Program will sleep for 3590 seconds and then start again

733edc5c79688b86a0b26177a4ec6e937410bcb6
Farqleet Ahmed Sheikh <farqleetahmedsheikh@users.noreply.github.com>
2024-11-27 02:01:03 +0500

    fix bugs and errors

--- whatsapp.py
+++ whatsapp.py
@@ -17,25 +17,14 @@ def input_msg():
         choice = input("Do you want to add another msg (y/n)").lower()
         os.system("clear")
 
-def send_msg():
-     # Running loop to send messages infinite times
-    while True:
-        num = random.randint(1,len(msgs))
-        msg = msgs[num]
-        pywhatkit.sendwhatmsg(number, msg, hour,0)
-        hour += 1
-        os.system("clear")
-        print("If you want to close the program press CTRL+C")
-        if hour == 24:
-            hour = 0
-        time.sleep(3590) # Program will sleep for 3590 seconds and then start again
+hour = time.localtime().tm_hour
 
 # Getting user local time
 currentTime = f"Current time is {time.localtime().tm_hour} : {time.localtime().tm_min}"
 
 print(currentTime) # Printing current local time
 
-hour = time.localtime().tm_hour + 1 # setting hours 
+# setting hours 
 
 # Getting data from user like number and message which to be send
 number = input("Enter Number with country code: (+923333333333) : ")
@@ -45,4 +34,15 @@ input_msg() # Calling function to add messages
 # Printing some information about message and others
 print(f"Your {msgs} messages will start transfer from {hour} : 00 to {number} Number")
 
-send_msg()
+# Running loop to send messages infinite times
+
+while True:
+    num = random.randint(2,len(msgs)-1)
+    msg = msgs[num]
+    pywhatkit.sendwhatmsg(number, msg, hour,58)
+    hour += 1
+    os.system("clear")
+    print("If you want to close the program press CTRL+C")
+    if hour == 24:
+        hour = 0
+    time.sleep(3590) # Program will sleep for 3590 seconds and then start again
